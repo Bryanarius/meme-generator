79ba27dc0f27c248c85796eda2a47a9d6eb37f9b
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-07-26 05:36:07 -0400

    Memes Generator V1
    - Adding top text and bottom text form
    
    - Implemented meme API using the useEffect function
    
    - Minor changes/ bug fixes

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,14 +2,37 @@ import React from "react"
 import memesData from "../memesData"
 
 export default function Meme() {
-const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
+
 
 
     function getMemeImage(){
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        setMemeImage(memesArray[randomNumber].url)
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage:url
+        }))
+    }
+
+        function handleChange(event){
+            const {name, value} = event.target
+            setMeme(prevMeme => ({
+                ...prevMeme,
+                [name]: value
+            }))
     }
     return (
         <main>
@@ -18,11 +41,17 @@ const [memeImage, setMemeImage] = React.useState("")
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input
                     type ="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button
                     className="form--button"
@@ -31,7 +60,11 @@ const [memeImage, setMemeImage] = React.useState("")
                     Get a new meme image 🖼
                 </button>
             </div>
-            <img src={memeImage} className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText} </h2>
+                <h2 className="meme--text bottom">{meme.bottomText} </h2>
+            </div>
         </main>
     )
 }
